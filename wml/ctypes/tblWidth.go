54c302693370268c9d661cb0589a112787cd4c56
evan.lu <luhaoran1210@mail.com>
2025-10-30 15:02:29 +0800

    fix tblW

--- wml/ctypes/tblWidth.go
+++ wml/ctypes/tblWidth.go
@@ -2,6 +2,7 @@ package ctypes
 
 import (
 	"encoding/xml"
+	"github.com/iEvan-lhr/docx-agent/internal"
 	"strconv"
 
 	"github.com/iEvan-lhr/docx-agent/wml/stypes"
@@ -9,13 +10,13 @@ import (
 
 // TableWidth represents the width of a table in a document.
 type TableWidth struct {
-	Width     *int               `xml:"w,attr,omitempty"`
+	Width     *string            `xml:"w,attr,omitempty"`
 	WidthType *stypes.TableWidth `xml:"type,attr,omitempty"`
 }
 
 func NewTableWidth(width int, widthType stypes.TableWidth) *TableWidth {
 	return &TableWidth{
-		Width:     &width,
+		Width:     internal.ToPtr(strconv.Itoa(width)),
 		WidthType: &widthType,
 	}
 }
@@ -23,7 +24,7 @@ func NewTableWidth(width int, widthType stypes.TableWidth) *TableWidth {
 func (t TableWidth) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
 
 	if t.Width != nil {
-		start.Attr = append(start.Attr, xml.Attr{Name: xml.Name{Local: "w:w"}, Value: strconv.Itoa(*t.Width)})
+		start.Attr = append(start.Attr, xml.Attr{Name: xml.Name{Local: "w:w"}, Value: *t.Width})
 	}
 
 	if t.WidthType != nil {
